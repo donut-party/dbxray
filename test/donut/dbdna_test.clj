33213b0e0575ce7914f31ac7dee21c6712484681
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-08-27 10:53:31 -0400

    condense main test case

--- test/donut/dbdna_test.clj
+++ test/donut/dbdna_test.clj
@@ -11,18 +11,24 @@
 (def test-dbconn (atom nil))
 (defonce embedded-pg (future (EmbeddedPostgres/start)))
 
-(def ^:private test-postgres {:dbtype "embedded-postgres" :dbname "dbdna_test"})
+
+(def ^:private test-h2         {:dbtype "h2" :dbname "dbdna_test" :user "root"})
+(def ^:private test-hsql       {:dbtype "hsql" :dbname "dbdna_test" :user "root"})
+(def ^:private test-mysql      {:dbtype "mysql" :dbname "dbdna_test" :user "root"})
+(def ^:private test-postgres   {:dbtype "embedded-postgres" :dbname "dbdna_test"})
 (def ^:private test-sqlite-mem {:dbtype "sqlite" :connection-uri "jdbc:sqlite::memory:"})
-(def ^:private test-sqlite-fs {:dbtype "sqlite" :dbname "sqlite.db"})
-(def ^:private test-mysql {:dbtype "mysql" :dbname "dbdna_test" :user "root"})
+(def ^:private test-sqlite-fs  {:dbtype "sqlite" :dbname "sqlite.db"})
 
 (def ^:dynamic *dbtype*)
 
 (def test-dbspecs
-  [test-postgres
+  [#_#_
+   test-h2
+   test-hsql
+   test-mysql
+   test-postgres
    test-sqlite-mem
-   test-sqlite-fs
-   test-mysql])
+   test-sqlite-fs])
 
 (defn build-stmt
   "helper to vary sql by db type"
@@ -43,52 +49,24 @@
 
 (defn create-tables
   [conn]
-  (doseq [table-name ["todos" "todo_lists" "users" #_#_"t2" "t1"]]
+  (doseq [table-name ["child_records" "parent_records"]]
     (try
       (jdbc/execute! conn [(str "DROP TABLE " table-name)])
       (catch Exception _)))
 
   (execute-many!
    conn
-   [["CREATE TABLE users ("
-     "  id integer PRIMARY KEY NOT NULL UNIQUE,"
-     "  username varchar(256) NOT NULL UNIQUE"
+   [["CREATE TABLE parent_records ("
+     "  id           integer PRIMARY KEY NOT NULL UNIQUE,"
+     "  varchar_ex   varchar(256) NOT NULL UNIQUE,"
+     "  text_ex      text,"
+     "  timestamp_ex TIMESTAMP NULL"
      ")"]
-    ["CREATE TABLE todo_lists ("
-     "  id integer PRIMARY KEY NOT NULL UNIQUE,"
-     "  created_by_id INTEGER,"
-     "  FOREIGN KEY(created_by_id)"
-     "    REFERENCES users(id)"
-     ")"]
-    ["CREATE TABLE todos ("
-     "   id integer PRIMARY KEY NOT NULL UNIQUE,"
-     "   todo_list_id INTEGER,"
-     "   todo_title varchar(256) NOT NULL,"
-     "   notes text,"
-     "   completed_at TIMESTAMP NULL,"
-     "   FOREIGN KEY(todo_list_id)"
-     "     REFERENCES todo_lists(id)"
-     ")"]]))
-
-(defn create-multi-fk-tables
-  [conn]
-  (doseq [table-name ["t2" "t1"]]
-    (try
-      (jdbc/execute! conn [(str "DROP TABLE " table-name)])
-      (catch Exception _)))
-  (execute-many!
-   conn
-   [["CREATE TABLE t1 ("
-     "   a INTEGER,"
-     "   b INTEGER,"
-     "   c INTEGER,"
-     "   PRIMARY KEY (a, b)"
-     ")"]
-    ["CREATE TABLE t2 ("
-     "   x INTEGER PRIMARY KEY,"
-     "   y INTEGER,"
-     "   z INTEGER,"
-     "   CONSTRAINT \"foo\" FOREIGN KEY (x, y) REFERENCES t1 (a, b)"
+    ["CREATE TABLE child_records ("
+     "  id    integer PRIMARY KEY NOT NULL UNIQUE,"
+     "  fk_id integer NOT NULL,"
+     "  FOREIGN KEY(fk_id)"
+     "    REFERENCES parent_records(id)"
      ")"]]))
 
 (defn with-test-db
@@ -119,61 +97,45 @@
 
 (deftest returns-tables
   (let [dna (dbd/dna @test-dbconn)]
-    (is (match? {:users      {:columns {:id       {:column-type  :integer
-                                                   :primary-key? true
-                                                   :unique?      true}
-                                        :username {:column-type :varchar
-                                                   :unique?     true}}}
-                 :todo_lists {:columns {:id            {:column-type  :integer
-                                                        :primary-key? true
-                                                        :unique?      true}
-                                        :created_by_id {:column-type :integer
-                                                        :nullable?   true
-                                                        :refers-to   [:users :id]}}}
-                 :todos      {:columns {:id           {:column-type  :integer
-                                                       :primary-key? true
-                                                       :unique?      true}
-                                        :todo_list_id {:column-type :integer
-                                                       :nullable?   true
-                                                       :refers-to   [:todo_lists :id]}
-                                        :todo_title   {:column-type :varchar}
-                                        :notes        {:column-type :text
-                                                       :nullable?   true}
-                                        :completed_at {:column-type :timestamp
-                                                       :nullable?   true}}}}
+    (is (match? {:parent_records {:columns {:id           {:column-type  :integer
+                                                           :primary-key? true
+                                                           :unique?      true}
+                                            :varchar_ex   {:column-type :varchar
+                                                           :unique?     true}
+                                            :text_ex      {:column-type :text
+                                                           :nullable?   true}
+                                            :timestamp_ex {:column-type :timestamp
+                                                           :nullable?   true}}}
+                 :child_records  {:columns {:id    {:column-type  :integer
+                                                    :primary-key? true
+                                                    :unique?      true}
+                                            :fk_id {:column-type :integer
+                                                    :refers-to   [:parent_records :id]}}}}
                 dna))
 
     (match-when-dbtype
      dna
-     [:postgresql {:users      {:columns {:id       {:raw-column-type "int4"}
-                                          :username {:raw-column-type "varchar"}}}
-                   :todo_lists {:columns {:id            {:raw-column-type "int4"}
-                                          :created_by_id {:raw-column-type "int4"}}}
-                   :todos      {:columns {:id           {:raw-column-type "int4"}
-                                          :todo_list_id {:raw-column-type "int4"}
-                                          :todo_title   {:raw-column-type "varchar"}
-                                          :notes        {:raw-column-type "text"}
-                                          :completed_at {:raw-column-type "timestamp"}}}}]
+     [:postgresql {:parent_records {:columns {:id           {:raw-column-type "int4"}
+                                              :varchar_ex   {:raw-column-type "varchar"}
+                                              :text_ex      {:raw-column-type "text"}
+                                              :timestamp_ex {:raw-column-type "timestamp"}}}
+                   :child_records  {:columns {:id    {:raw-column-type "int4"}
+                                              :fk_id {:raw-column-type "int4"}}}}]
 
-     [:mysql {:users      {:columns {:id       {:raw-column-type "INT"}
-                                     :username {:raw-column-type "VARCHAR"}}}
-              :todo_lists {:columns {:id            {:raw-column-type "INT"}
-                                     :created_by_id {:raw-column-type "INT"}}}
-              :todos      {:columns {:id           {:raw-column-type "INT"}
-                                     :todo_list_id {:raw-column-type "INT"}
-                                     :todo_title   {:raw-column-type "VARCHAR"}
-                                     :notes        {:raw-column-type "TEXT"}
-                                     :completed_at {:raw-column-type "TIMESTAMP"}}}}]
+     [:mysql {:parent_records {:columns {:id           {:raw-column-type "INT"}
+                                         :varchar_ex   {:raw-column-type "VARCHAR"}
+                                         :text_ex      {:raw-column-type "TEXT"}
+                                         :timestamp_ex {:raw-column-type "TIMESTAMP"}}}
+              :child_records  {:columns {:id    {:raw-column-type "INT"}
+                                         :fk_id {:raw-column-type "INT"}}}}]
 
-     [:sqlite {:users      {:columns {:id       {:raw-column-type "INTEGER"}
-                                      :username {:raw-column-type "VARCHAR(256)"}}}
-               :todo_lists {:columns {:id            {:raw-column-type "INTEGER"}
-                                      :created_by_id {:raw-column-type "INTEGER"}}}
-               :todos      {:columns {:id           {:raw-column-type "INTEGER"}
-                                      :todo_list_id {:raw-column-type "INTEGER"}
-                                      :todo_title   {:raw-column-type "VARCHAR(256)"}
-                                      :notes        {:raw-column-type "TEXT"}
-                                      :completed_at {:raw-column-type "TIMESTAMP"}}}}])))
+     [:sqlite {:parent_records {:columns {:id           {:raw-column-type "INTEGER"}
+                                          :varchar_ex   {:raw-column-type "VARCHAR(256)"}
+                                          :text_ex      {:raw-column-type "TEXT"}
+                                          :timestamp_ex {:raw-column-type "TIMESTAMP"}}}
+               :child_records  {:columns {:id    {:raw-column-type "INTEGER"}
+                                          :fk_id {:raw-column-type "INTEGER"}}}}])))
+
 
 (comment
   (do
@@ -194,3 +156,49 @@
   (dbd/explore [md pg-conn]
                (.getImportedKeys md nil nil "todos"))
   )
+
+
+(comment
+  (execute-many!
+   conn
+   [["CREATE TABLE users ("
+     "  id integer PRIMARY KEY NOT NULL UNIQUE,"
+     "  username varchar(256) NOT NULL UNIQUE"
+     ")"]
+    ["CREATE TABLE todo_lists ("
+     "  id integer PRIMARY KEY NOT NULL UNIQUE,"
+     "  created_by_id INTEGER,"
+     "  FOREIGN KEY(created_by_id)"
+     "    REFERENCES users(id)"
+     ")"]
+    ["CREATE TABLE todos ("
+     "   id integer PRIMARY KEY NOT NULL UNIQUE,"
+     "   todo_list_id INTEGER,"
+     "   todo_title varchar(256) NOT NULL,"
+     "   notes text,"
+     "   completed_at TIMESTAMP NULL,"
+     "   FOREIGN KEY(todo_list_id)"
+     "     REFERENCES todo_lists(id)"
+     ")"]]))
+
+(comment
+  (defn create-multi-fk-tables
+    [conn]
+    (doseq [table-name ["t2" "t1"]]
+      (try
+        (jdbc/execute! conn [(str "DROP TABLE " table-name)])
+        (catch Exception _)))
+    (execute-many!
+     conn
+     [["CREATE TABLE t1 ("
+       "   a INTEGER,"
+       "   b INTEGER,"
+       "   c INTEGER,"
+       "   PRIMARY KEY (a, b)"
+       ")"]
+      ["CREATE TABLE t2 ("
+       "   x INTEGER PRIMARY KEY,"
+       "   y INTEGER,"
+       "   z INTEGER,"
+       "   CONSTRAINT \"foo\" FOREIGN KEY (x, y) REFERENCES t1 (a, b)"
+       ")"]])))
